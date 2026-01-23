58b6f7afdd539968fbb9dd13905aa9cf6e010665
Pavan Katre <pavankatre@gmail.com>
2026-01-23 11:07:57 +0530

    feat: implement header component with material toolbar

---
+++ src/app/shared/ui/header/header.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+
+}
