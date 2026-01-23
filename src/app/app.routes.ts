e4e6b943e9246fc5a866de7c0ce390335076d8e3
Pavan Katre <pavankatre@gmail.com>
2026-01-23 11:16:20 +0530

    feat: create dashboard component and configure initial routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from '../app/features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: Dashboard }, // Default page
+  { path: '**', redirectTo: '' }
+];
