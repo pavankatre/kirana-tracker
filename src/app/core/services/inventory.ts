0ecc9318788afcf67bae5740fdf6e96e59eea452
Pavan Katre <pavankatre@gmail.com>
2026-01-23 13:05:48 +0530

    feat: add mock product data for dairy, grains, and household categories

--- src/app/core/services/inventory.ts
+++ src/app/core/services/inventory.ts
@@ -5,21 +5,53 @@ import { KiranaItem } from '../../models/kirana-item';
 export class InventoryService {
   private itemsSignal = signal<KiranaItem[]>([
     // Adding sample data so you can see it on screen immediately
-    { 
+  { 
     id: '1', 
     name: 'Lux Soap', 
     category: 'Personal Care', 
     stockCount: 5, 
     minThreshold: 1,
-    lastUpdated: new Date() // <--- Add this line
+    lastUpdated: new Date() 
   },
   { 
     id: '2', 
     name: 'Sugar', 
     category: 'Groceries', 
-    stockCount: 1, 
+    stockCount: 1, // Will show as Low Stock
     minThreshold: 1,
-    lastUpdated: new Date() // <--- Add this line
+    lastUpdated: new Date() 
+  },
+  { 
+    id: '3', 
+    name: 'Amul Milk (1L)', 
+    category: 'Dairy', 
+    stockCount: 10, 
+    minThreshold: 3,
+    lastUpdated: new Date() 
+  },
+  { 
+    id: '4', 
+    name: 'Basmati Rice (5kg)', 
+    category: 'Grains', 
+    stockCount: 2, // Will show as Low Stock
+    minThreshold: 3,
+    lastUpdated: new Date() 
+  },
+  { 
+    id: '5', 
+    name: 'Tata Salt', 
+    category: 'Groceries', 
+    stockCount: 8, 
+    minThreshold: 1,
+    lastUpdated: new Date() 
+  },
+  { 
+    id: '6', 
+    name: 'Dishwash Liquid', 
+    category: 'Household', 
+    stockCount: 0, // Out of Stock!
+    minThreshold: 1,
+    lastUpdated: new Date() 
   }
   ]);
 
