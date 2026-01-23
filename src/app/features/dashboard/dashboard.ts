5e75bafa9184e9d2496c1d2ad06e3f87bc3a2d1d
Pavan Katre <pavankatre@gmail.com>
2026-01-23 17:06:55 +0530

    feat(dashboard): integrate table and service into dashboard UI
    - Connected DashboardTableService to the Dashboard component
    - Implemented responsive layout with search and category filter controls
    - Integrated the reusable app-item-table with paginated data binding
    - Added modern SCSS styling including card elevations and hover effects

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DashboardTableService } from './services/dashboard-table';
+import { ItemTableComponent } from '../../shared/components/item-table/item-table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ItemTableComponent,MatCardModule, MatSelectModule, MatFormFieldModule, MatInputModule , MatIconModule],
+  providers: [DashboardTableService],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
-
+protected tableService = inject(DashboardTableService);
 }
