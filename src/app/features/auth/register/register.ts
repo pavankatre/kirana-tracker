c701d44c4af6e8aece0b1c319942ed1063620ca3
Pavan Katre <pavankatre@gmail.com>
2026-01-26 15:37:58 +0530

    feat(auth): implement login, registration, and signal-based auth guard

---
+++ src/app/features/auth/register/register.ts
@@ -0,0 +1,46 @@
+
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth/auth';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, ReactiveFormsModule,MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule ], 
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+
+
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  }, { validators: this.passwordMatchValidator });
+
+  // Custom Validator for matching passwords
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    return password && confirmPassword && password.value !== confirmPassword.value 
+      ? { passwordMismatch: true } : null;
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value);
+    }
+  }
+
+}
