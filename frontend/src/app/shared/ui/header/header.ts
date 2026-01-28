45327c13e9695f736a5edc50e73edf6c19778d7f
Pavan Katre <pavankatre@gmail.com>
2026-01-29 01:54:08 +0530

    feat: add user name display and logout functionality to header

--- frontend/src/app/shared/ui/header/header.ts
+++ frontend/src/app/shared/ui/header/header.ts
@@ -1,17 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserStorageService } from '../../../core/services/storage/user-storage';
+import { AuthService } from '../../../core/services/auth/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink ,MatMenuModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink ,MatMenuModule , MatDividerModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnInit {
 
+  storeService=inject(UserStorageService);
+  authService=inject(AuthService);
+  router=inject(Router);
+
+currentUser = signal<string | null>(localStorage.getItem('user.username'));
+
+ngOnInit() {
+  this.currentUser.set( this.storeService.getUserFromStorage().username);
+}
+
+onLogout() {
+  
+  this.authService.logout();
+  this.currentUser.set(null);
+  this.router.navigate(['/login']);
+}
 }
